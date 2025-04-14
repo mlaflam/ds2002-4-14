c8174a4ca820856cb3c57836bda20edf2fc88299
Margaret Laflam <118241749+mlaflam@users.noreply.github.com>
2025-04-14 20:20:05 +0000

    Docker example

---
+++ iss.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import datetime
+
+url = "http://api.open-notify.org/iss-now.json"
+
+response = requests.get(url)
+r = response.json()
+
+timestamp = r['timestamp']
+# convert timestamp to human readable date and time 
+datetime = datetime.datetime.fromtimestamp(timestamp)
+dtime = datetime.strftime('%Y-%m-%d, %H:%M:%S')
+
+long = r['iss_position']['longitude']
+lat = r['iss_position']['latitude']
+
+print(dtime)
+print(long)
+print(lat)
+
+lines = [dtime, "\n", long, "\n", lat]
+
+with open('/data/output.txt', 'w') as f:
+    f.writelines(lines)
